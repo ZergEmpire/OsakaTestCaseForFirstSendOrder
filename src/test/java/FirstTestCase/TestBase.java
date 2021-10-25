2bc7e566cd3feaae8536d2dd30132621cc22ae75
ZergEmpire <71401820+ZergEmpire@users.noreply.github.com>
2021-10-25 11:47:33 +0300

    Commit
    Добавлен новый порт для подключения к Moon
    selenoid options заменён на Moon options

--- src/test/java/FirstTestCase/TestBase.java
+++ src/test/java/FirstTestCase/TestBase.java
@@ -46,14 +46,15 @@ public class TestBase {
         capabilities.setCapability("browserName", "chrome");
         capabilities.setCapability("browserVersion", "91.0");
 
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+        capabilities.setCapability("moon:options", Map.<String, Object>of(
                 "enableVNC", true,
                 "enableVideo", true
+
         ));
         RemoteWebDriver driver = null;
         try {
             driver = new RemoteWebDriver(
-                    new URL("http://192.168.1.18:8080/wd/hub"),
+                    new URL("http://192.168.1.17:30901/wd/hub"),
                     capabilities
             );
         } catch (MalformedURLException e) {
@@ -62,7 +63,7 @@ public class TestBase {
 
         WebDriverRunner.setWebDriver(driver);
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(70, TimeUnit.SECONDS);
 
     }
 
@@ -153,7 +154,7 @@ public class TestBase {
 
     @Step("Жду перехода в статус принят")
     public void waitForComplete() {
-        $x("//span[contains(text(),'Принят') or (contains(text(),'Поступил')) ]").shouldBe(visible);
+        $x("//span[contains(text(), \"Принят\") or (contains(text(), \"Поступил\"))]").shouldBe(visible);
     }
 
 
